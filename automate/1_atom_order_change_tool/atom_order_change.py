14b40dcb38faf80d76f2002660c90906c91123f2
Kee-Wang <qfwkee@gmail.com>
2016-11-07 15:57:31 -0500

    read files Ok

--- automate/1_atom_order_change_tool/atom_order_change.py
+++ automate/1_atom_order_change_tool/atom_order_change.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import shlex
 import numpy as np
+import json
 def cl(command):
     #ip::string, command line as string input
     #op::string, return value is the output of command line
@@ -14,15 +15,59 @@ def cl(command):
     print(output)
     return output
 
+configuration = list()
 #train_x = raw_input('Please input the data file name: ')
-train_x = 'pts.dat'
+train_x = 'pts.dat'#'testpoint_v2b_co2h2o.dat' #'pts.dat'
 f = open(train_x)
-nol = 0
+i=0
+energy = list()
+energy_cout = 0
+
+
+count_config = 1
 for line in f:
-  nol=nol+1
-  if nol==1:
-    natom=int(line)
-nconfig = nol/(natom+2)
+    i=i+1
+    if i==1:
+        natom = int(line)
+        height = natom + 2
+
+    lineinconfig = (i-1) % height + 1
+
+    if lineinconfig == 1:
+        molecule_coord = list() #Renew molecue_coord each configuration
+
+    if lineinconfig == 2:
+        energy = float(line.split()[0]) #Recond the energy. This can be dipole
+        dipole = [float(dip) for dip in line.split()[1:]]
+
+    if lineinconfig >= 3 and lineinconfig <= natom+2:
+        atom_n = lineinconfig - 2
+        element = line.split()[0]
+        coordinate = [float(coor) for coor in line.split()[1:]]#Read cooridnates and turns into float
+        atom_coord = [element,coordinate]
+        molecule_coord.append(atom_coord)
+
+    if lineinconfig == height: #Reset some of the values
+        configuration.append([[[natom],[energy, dipole]],molecule_coord])
+
+        if count_config == 10:
+            print('Lines: '+str(i))
+            print('Configuration: '+str(count_config))
+            break
+        count_config = count_config + 1
 f.close()
 
+#f=open('result','w')
+for a in configuration: #To print in column
+    for b in a:
+        print(str(b[0][0]))
+        print(str(b[1][0]))
+        for i in b[1][1]:
+            print(i)
+a = np.array(configuration)
 
+print (','.join(configuration))
+#            f.write(str(c[0])+'\n')
+#f.close()
+
+print('test over')
