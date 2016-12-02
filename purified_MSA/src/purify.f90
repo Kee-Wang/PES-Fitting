2593054b91cbe4fc380a76e75a1c7abe5465f8e7
Kee-Wang <kee@macronode.chem.emory.edu>
2016-12-02 11:53:46 -0500

    purified

---
+++ purified_MSA/src/purify.f90
@@ -0,0 +1,30 @@
+program purify
+use basis
+
+real,dimension(1:15)::x
+real,dimension(0:5834)::p
+integer::i
+x(1)=2
+x(2)=0
+x(3)=0
+x(4)=0
+x(5)=3
+x(6)=0
+x(7)=0
+x(8)=0
+x(9)=5
+x(10)=7
+X(11)=11
+X(12)=0
+X(13)=13
+X(14)=0
+X(15)=0
+
+call bemsav(x,p)
+open(file='purified_coeff.dat',status='unknown',unit=11)
+
+do i=1,5835
+write(11,'(F20.10)') p(i-1)
+end do
+
+end
